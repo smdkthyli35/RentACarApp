1c6b3b31319749b44368c376eaac1a2e1afba4a5
zeljkosamed <smdkthyli@gmail.com>
2022-04-05 21:38:51 +0300

    Entities sınıfları oluşturuldu.

---
+++ src/rentACar/Domain/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car : Entity
+    {
+        public int ColorId { get; set; }
+        public int ModelId { get; set; }
+        public string Plate { get; set; }
+        public short ModelYear { get; set; }
+    }
+}
---
+++ src/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Model : Entity
+    {
+        public string Name { get; set; }
+        public double DailyPrice { get; set; }
+        public int TransmissionId { get; set; }
+        public int FuelId { get; set; }
+        public int BrandId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
