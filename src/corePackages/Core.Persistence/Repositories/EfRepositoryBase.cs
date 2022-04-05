6a6492f874b1ef48753e209722e8e6a68e245cc2
zeljkosamed <smdkthyli@gmail.com>
2022-04-05 22:56:34 +0300

    EfRepositoryBase oluşturuldu.

---
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,70 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public class EfRepositoryBase<TEntity, TContext> : IAsyncRepository<TEntity>
+        where TEntity : Entity
+        where TContext : DbContext
+    {
+        protected TContext _context { get; }
+
+        public EfRepositoryBase(TContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<IPaginate<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null, int index = 0, int size = 10, bool enableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<TEntity> queryable = Query();
+
+            if (!enableTracking)
+                queryable = queryable.AsNoTracking();
+            if (include != null)
+                queryable = include(queryable);
+            if (predicate != null)
+                queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).ToPaginateAsync(index, size, 0, cancellationToken);
+
+            return await queryable.ToPaginateAsync(index, size, 0, cancellationToken);
+        }
+
+        public IQueryable<TEntity> Query()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
