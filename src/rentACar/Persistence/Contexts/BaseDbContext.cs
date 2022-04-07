595df3b1d1420b87fa252e002f996de8b004c083
zeljkosamed <smdkthyli@gmail.com>
2022-04-07 15:21:46 +0300

    BaseDbContext oluşturuldu.

---
+++ src/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Fuel> Fuels { get; set; }
+        public DbSet<Transmission> Transmissions { get; set; }
+
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
