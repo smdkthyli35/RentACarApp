5c2134fbce4b8f2216407082f9be7feadb4f3095
zeljkosamed <smdkthyli@gmail.com>
2022-04-07 16:13:06 +0300

    PersistenceServiceRegistration oluşturuldu.

--- src/rentACar/Persistence/Repositories/FuelRepository.cs
+++ src/rentACar/Persistence/Repositories/FuelRepository.cs
@@ -1,4 +1,5 @@
-﻿using Core.Persistence.Repositories;
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
 using Domain.Entities;
 using Persistence.Contexts;
 using System;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Persistence.Repositories
 {
-    public class FuelRepository : EfRepositoryBase<Fuel, BaseDbContext>
+    public class FuelRepository : EfRepositoryBase<Fuel, BaseDbContext>, IFuelRepository
     {
         public FuelRepository(BaseDbContext context) : base(context)
         {
