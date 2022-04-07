8b6f6cbe9e15224cbf67ded3cea27260650f4c4e
zeljkosamed <smdkthyli@gmail.com>
2022-04-07 16:44:15 +0300

    BaseController ve BrandsController add metodu oluşturuldu.

---
+++ src/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            var result = await Mediator.Send(createBrandCommand);
+            return Created("", result);
+        }
+
+    }
+}
