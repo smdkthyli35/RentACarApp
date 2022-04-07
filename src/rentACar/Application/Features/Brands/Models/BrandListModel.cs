0ec17f34bb2f2524faefffd6cc82617200dd2e9a
zeljkosamed <smdkthyli@gmail.com>
2022-04-07 22:41:02 +0300

    BasePageableModel, BrandListDto, BrandListModel oluşturuldu.

---
+++ src/rentACar/Application/Features/Brands/Models/BrandListModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Brands.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Models
+{
+    public class BrandListModel
+    {
+        public IList<BrandListDto> Items { get; set; }
+    }
+}
