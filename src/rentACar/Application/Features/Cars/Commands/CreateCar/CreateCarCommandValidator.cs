223cb0617c23a39be8b9daa9bc46a582a66f0233
zeljkosamed <smdkthyli@gmail.com>
2022-04-08 19:02:13 +0300

    CarCustomValidationRules ve CreateCarCommandValidator eklendi.

---
+++ src/rentACar/Application/Features/Cars/Commands/CreateCar/CreateCarCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Cars.Validations;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cars.Commands.CreateCar
+{
+    public class CreateCarCommandValidator : AbstractValidator<CreateCarCommand>
+    {
+        public CreateCarCommandValidator()
+        {
+            RuleFor(c => c.ModelYear).GreaterThan((short)1900);
+            RuleFor(c => c.Plate).NotEmpty().Must(CarCustomValidationRules.IsTurkeyPlate).WithMessage("Lütfen geçerli bir plaka giriniz.");
+        }
+    }
+}
