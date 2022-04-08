f0a49bf24dcc3670b19e12f12bcb0e5c8b75821d
zeljkosamed <smdkthyli@gmail.com>
2022-04-08 21:16:03 +0300

    CarsController metodları yazıldı ve Car mapping sınıfı güncellendi.

--- src/rentACar/Application/Features/Cars/Profiles/MappingProfiles.cs
+++ src/rentACar/Application/Features/Cars/Profiles/MappingProfiles.cs
@@ -1,5 +1,10 @@
-﻿using Application.Features.Cars.Dtos;
+﻿using Application.Features.Cars.Commands.CreateCar;
+using Application.Features.Cars.Commands.DeleteCar;
+using Application.Features.Cars.Commands.UpdateCar;
+using Application.Features.Cars.Dtos;
+using Application.Features.Cars.Models;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -14,8 +19,13 @@ namespace Application.Features.Cars.Profiles
         public MappingProfiles()
         {
             CreateMap<Car, CreateCarDto>().ReverseMap();
+            CreateMap<Car, CreateCarCommand>().ReverseMap();
             CreateMap<Car, DeleteCarDto>().ReverseMap();
+            CreateMap<Car, DeleteCarCommand>().ReverseMap();
             CreateMap<Car, UpdateCarDto>().ReverseMap();
+            CreateMap<Car, UpdateCarCommand>().ReverseMap();
+            CreateMap<Car, CarListDto>().ReverseMap();
+            CreateMap<IPaginate<Car>, CarListModel>().ReverseMap();
         }
     }
 }
---
+++ src/rentACar/WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,49 @@
+﻿using Application.Features.Cars.Commands.CreateCar;
+using Application.Features.Cars.Commands.DeleteCar;
+using Application.Features.Cars.Commands.UpdateCar;
+using Application.Features.Cars.Queries.GetCarList;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : BaseController
+    {
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll([FromQuery] PageRequest pageRequest)
+        {
+            var query = new GetCarListQuery();
+            query.PageRequest = pageRequest;
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CreateCarCommand createCarCommand)
+        {
+            var result = await Mediator.Send(createCarCommand);
+            return Created("", result);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteCarCommand deleteCarCommand)
+        {
+            var result = await Mediator.Send(deleteCarCommand);
+            return Ok(result);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateCarCommand updateCarCommand)
+        {
+            var result = await Mediator.Send(updateCarCommand);
+            return Ok(result);
+        }
+    }
+}
