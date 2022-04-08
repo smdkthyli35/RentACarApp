7a388092480fb5409d9957f13039ba14292eaeac
zeljkosamed <smdkthyli@gmail.com>
2022-04-08 19:08:14 +0300

    DeleteCarCommand eklendi.

---
+++ src/rentACar/Application/Features/Cars/Commands/DeleteCar/DeleteCarCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Cars.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cars.Commands.DeleteCar
+{
+    public class DeleteCarCommand : IRequest<DeleteCarDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, DeleteCarDto>
+        {
+            private readonly ICarRepository _carRepository;
+            private readonly IMapper _mapper;
+
+            public DeleteCarCommandHandler(ICarRepository carRepository, IMapper mapper)
+            {
+                _carRepository = carRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<DeleteCarDto> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+            {
+                var carToBeDeleted = await _carRepository.GetAsync(c => c.Id == request.Id);
+                if (carToBeDeleted is null)
+                    throw new BusinessException("Böyle bir araba bulunamadı!");
+
+                Car mappedCar = _mapper.Map<Car>(request);
+                var deletedCar = _carRepository.DeleteAsync(mappedCar);
+                DeleteCarDto deleteCarDto = _mapper.Map<DeleteCarDto>(deletedCar);
+                return deleteCarDto;
+            }
+        }
+    }
+}
