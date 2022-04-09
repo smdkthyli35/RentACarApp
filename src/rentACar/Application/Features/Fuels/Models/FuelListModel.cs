47a9dc1a2dcd9ff9cfb731c302db251b4273d11d
zeljkosamed <smdkthyli@gmail.com>
2022-04-09 21:00:56 +0300

    Transmission, Fuel, Color için dto ve model sınıfları oluşturuldu.

---
+++ src/rentACar/Application/Features/Fuels/Models/FuelListModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Fuels.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Fuels.Models
+{
+    public class FuelListModel
+    {
+        public IList<FuelListDto> Items { get; set; }
+    }
+}
---
+++ src/rentACar/Application/Features/Transmissions/Dtos/DeleteTransmissionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Transmissions.Dtos
+{
+    public class DeleteTransmissionDto
+    {
+        public int Id { get; set; }      
+    }
+}
