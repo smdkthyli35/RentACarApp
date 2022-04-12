6196cd0eff78cdc9d0ff6d199bc9b0c20f42091c
zeljkosamed <smdkthyli@gmail.com>
2022-04-12 21:05:34 +0300

    ICachableRequest ve CachingBehavior eklendi.

---
+++ src/corePackages/Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CacheSettings
+    {
+        public int SlidingExpiration { get; set; }
+    }
+
+    public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : ICachableRequest, IRequest<TResponse>
+    {
+        private readonly IDistributedCache _cache;
+        private readonly ILogger _logger;
+
+        public CachingBehavior(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            TResponse response;
+            if (request.BypassCache) return await next();
+
+            async Task<TResponse> GetResponseAndAddToCache()
+            {
+                response = await next();
+
+                var slidingExpiration = request.SlidingExpiration == null ? TimeSpan.FromHours(2) : request.SlidingExpiration;
+
+                var cacheOptions = new DistributedCacheEntryOptions
+                {
+                    SlidingExpiration = slidingExpiration
+                };
+
+                var serializedData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response));
+
+                await _cache.SetAsync(request.CacheKey, serializedData, cacheOptions, cancellationToken);
+                return response;
+            }
+
+            var cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
+            if (cachedResponse != null)
+            {
+                response = JsonConvert.DeserializeObject<TResponse>(Encoding.Default.GetString(cachedResponse));
+            }
+            else
+            {
+                response = await GetResponseAndAddToCache();
+            }
+
+            return response;
+        }
+    }
+}
