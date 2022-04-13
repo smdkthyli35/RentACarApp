feb4fb8fc4765f6467d68ca920dfc86a02528e38
zeljkosamed <smdkthyli@gmail.com>
2022-04-13 15:11:59 +0300

    Yeni eklenen sınıfların interface ve class repositoryleri oluşturuldu.

---
+++ src/rentACar/Persistence/Repositories/InvidualCustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class InvidualCustomerRepository : EfRepositoryBase<InvidualCustomer, BaseDbContext>, IInvidualCustomerRepository
+    {
+        public InvidualCustomerRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
