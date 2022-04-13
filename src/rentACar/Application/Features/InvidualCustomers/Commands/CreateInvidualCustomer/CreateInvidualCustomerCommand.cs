3c80e0cbd2e0a6791a54e0d9b2c9e67fd0b339bb
zeljkosamed <smdkthyli@gmail.com>
2022-04-13 21:27:43 +0300

    InvidualCustomer create, update, delete commandleri eklendi.

---
+++ src/rentACar/Application/Features/InvidualCustomers/Commands/CreateInvidualCustomer/CreateInvidualCustomerCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.InvidualCustomers.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.InvidualCustomers.Commands.CreateInvidualCustomer
+{
+    public class CreateInvidualCustomerCommand : IRequest<CreateInvidualCustomerDto>
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentity { get; set; }
+
+        public class CreateInvidualCustomerCommandHandler : IRequestHandler<CreateInvidualCustomerCommand, CreateInvidualCustomerDto>
+        {
+            private readonly IInvidualCustomerRepository _invidualCustomerRepository;
+            private readonly IMapper _mapper;
+
+            public CreateInvidualCustomerCommandHandler(IInvidualCustomerRepository invidualCustomerRepository, IMapper mapper)
+            {
+                _invidualCustomerRepository = invidualCustomerRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreateInvidualCustomerDto> Handle(CreateInvidualCustomerCommand request, CancellationToken cancellationToken)
+            {
+                InvidualCustomer mappedInvidualCustomer = _mapper.Map<InvidualCustomer>(request);
+                InvidualCustomer createdInvidualCustomer = await _invidualCustomerRepository.AddAsync(mappedInvidualCustomer);
+                CreateInvidualCustomerDto createInvidualCustomerDto = _mapper.Map<CreateInvidualCustomerDto>(createdInvidualCustomer);
+                return createInvidualCustomerDto;
+            }
+        }
+    }
+}
---
+++ src/rentACar/Application/Features/InvidualCustomers/Commands/DeleteInvidualCustomer/DeleteInvidualCustomerCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.InvidualCustomers.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.InvidualCustomers.Commands.DeleteInvidualCustomer
+{
+    public class DeleteInvidualCustomerCommand : IRequest<DeleteInvidualCustomerDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteInvidualCustomerCommandHandler : IRequestHandler<DeleteInvidualCustomerCommand, DeleteInvidualCustomerDto>
+        {
+            private readonly IInvidualCustomerRepository _invidualCustomerRepository;
+            private readonly IMapper _mapper;
+
+            public DeleteInvidualCustomerCommandHandler(IInvidualCustomerRepository invidualCustomerRepository, IMapper mapper)
+            {
+                _invidualCustomerRepository = invidualCustomerRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<DeleteInvidualCustomerDto> Handle(DeleteInvidualCustomerCommand request, CancellationToken cancellationToken)
+            {
+                var invidualCustomerToBeDeleted = await _invidualCustomerRepository.GetAsync(i => i.Id == request.Id);
+                if (invidualCustomerToBeDeleted is null)
+                    throw new BusinessException("Böyle bir bireysel müşteri bulunamadı.");
+
+                InvidualCustomer mappedInvidualCustomer = _mapper.Map<InvidualCustomer>(request);
+                var deletedInvidualCustomer = _invidualCustomerRepository.DeleteAsync(mappedInvidualCustomer);
+                DeleteInvidualCustomerDto deleteInvidualCustomerDto = _mapper.Map<DeleteInvidualCustomerDto>(deletedInvidualCustomer);
+                return deleteInvidualCustomerDto;
+            }
+        }
+    }
+}
