01f0b8e149d48603ff84c16d897079001aac6086
zeljkosamed <smdkthyli@gmail.com>
2022-04-13 21:42:32 +0300

    InvidualCustomer query, business rule ve mapping sınıfı oluşturuldu.

---
+++ src/rentACar/Application/Features/InvidualCustomers/Queries/GetInvidualCustomerList/GetInvidualCustomerListQuery.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.InvidualCustomers.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Pipelines.Caching;
+using Core.Application.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.InvidualCustomers.Queries.GetInvidualCustomerList
+{
+    public class GetInvidualCustomerListQuery : IRequest<InvidualCustomerListModel>, ICachableRequest
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public bool BypassCache { get; set; }
+        public string CacheKey => "invidual-customers-list";
+        public TimeSpan? SlidingExpiration { get; set; }
+
+        public class GetInvidualCustomerListQueryHandler : IRequestHandler<GetInvidualCustomerListQuery, InvidualCustomerListModel>
+        {
+            private readonly IInvidualCustomerRepository _invidualCustomerRepository;
+            private readonly IMapper _mapper;
+
+            public GetInvidualCustomerListQueryHandler(IInvidualCustomerRepository invidualCustomerRepository, IMapper mapper)
+            {
+                _invidualCustomerRepository = invidualCustomerRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<InvidualCustomerListModel> Handle(GetInvidualCustomerListQuery request, CancellationToken cancellationToken)
+            {
+                var invidualCustomers = await _invidualCustomerRepository.GetListAsync(
+                    index: request.PageRequest.Page,
+                    size: request.PageRequest.PageSize);
+
+                var mappedInvidualCustomers = _mapper.Map<InvidualCustomerListModel>(invidualCustomers);
+                return mappedInvidualCustomers;
+            }
+        }
+    }
+}
---
+++ src/rentACar/Application/Features/InvidualCustomers/Rules/InvidualCustomerBusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.InvidualCustomers.Rules
+{
+    public class InvidualCustomerBusinessRules
+    {
+        private readonly IInvidualCustomerRepository _invidualCustomerRepository;
+
+        public InvidualCustomerBusinessRules(IInvidualCustomerRepository invidualCustomerRepository)
+        {
+            _invidualCustomerRepository = invidualCustomerRepository;
+        }
+
+        public async Task NationalIdentityCanBotBeDublicated(string nationalIdentity)
+        {
+            var result = await _invidualCustomerRepository.GetListAsync(i => i.NationalIdentity == nationalIdentity);
+            if (result.Items.Any())
+            {
+                throw new BusinessException("Bu kimnlik numarası daha önceden eklenmiş. Lütfen farklı bir kimnlik numarası giriniz.");
+            }
+        }
+    }
+}
