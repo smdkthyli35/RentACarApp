951ec7fd1fe684fe88a76d3dfc74ad02beb88655
zeljkosamed <smdkthyli@gmail.com>
2022-04-15 16:34:20 +0300

    AdditionalService create ve update validatorları eklendi.

---
+++ src/rentACar/Application/Features/AdditionalServices/Commands/CreateAdditionalService/CreateAdditionalServiceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AdditionalServices.Commands.CreateAdditionalService
+{
+    public class CreateAdditionalServiceCommandValidator : AbstractValidator<CreateAdditionalServiceCommand>
+    {
+        public CreateAdditionalServiceCommandValidator()
+        {
+            RuleFor(a => a.Name).NotEmpty();
+            RuleFor(a => a.Name).MaximumLength(100);
+        }
+    }
+}
---
+++ src/rentACar/Application/Features/AdditionalServices/Commands/UpdateAdditionalService/UpdateAdditionalServiceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AdditionalServices.Commands.UpdateAdditionalService
+{
+    public class UpdateAdditionalServiceCommandValidator : AbstractValidator<UpdateAdditionalServiceCommand>
+    {
+        public UpdateAdditionalServiceCommandValidator()
+        {
+            RuleFor(a => a.Name).NotEmpty();
+            RuleFor(a => a.Name).MaximumLength(100);
+        }
+    }
+}
