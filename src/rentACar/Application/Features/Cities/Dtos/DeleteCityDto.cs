002f4a7eaa4e3bb0cc551dc331f8f8958192f96c
zeljkosamed <smdkthyli@gmail.com>
2022-04-15 15:22:09 +0300

    City dtos ve model sınıfları oluşturuldu.

---
+++ src/rentACar/Application/Features/Cities/Dtos/DeleteCityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cities.Dtos
+{
+    public class DeleteCityDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/rentACar/Application/Features/Cities/Models/CityListModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Cities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Cities.Models
+{
+    public class CityListModel
+    {
+        public IList<CityListDto> Items { get; set; }
+    }
+}
