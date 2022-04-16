be3fa30e98b5f621454c7aa364f1c1d5f5d66118
zeljkosamed <smdkthyli@gmail.com>
2022-04-16 14:30:26 +0300

    Invoice dtos ve model sınıfları oluşturuldu.

---
+++ src/rentACar/Application/Features/Invoices/Dtos/DeleteInvoiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Invoices.Dtos
+{
+    public class DeleteInvoiceDto
+    {
+        public int Id { get; set; }
+        public string InvoiceNo { get; set; }
+    }
+}
---
+++ src/rentACar/Application/Features/Invoices/Dtos/InvoiceListDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Invoices.Dtos
+{
+    public class InvoiceListDto
+    {
+        public int Id { get; set; }
+        public int CustomerEmail { get; set; }
+        public string InvoiceNo { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime RentalStartDate { get; set; }
+        public DateTime RentalEndDate { get; set; }
+        public float RentalDayCount { get; set; }
+        public decimal TotalRentalPrice { get; set; }
+    }
+}
